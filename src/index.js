8969f3be4a0cb835b9f41abb516f4765cd1290f1
Sambhu2612 <piyush.mitra87@gmail.com>
2021-09-08 19:10:54 +0530

    Mobile First Approach Complete

---
+++ src/index.js
@@ -0,0 +1,86 @@
+setTimeout(() => {
+    document.querySelector("body div:nth-child(3) h1").innerText = "Order from top & favourite restaurants";
+    document.querySelector("body div:nth-child(3) h1").style = "opacity:100";
+}, 500);
+setTimeout(() => {
+    document.querySelector("div.container").style = "transform:translateX(-100%)";
+    document.querySelector("body div:nth-child(2) button:nth-child(1)").style = "background-color: rgba(255, 255, 255, .5);";
+    document.querySelector("body div:nth-child(2) button:nth-child(3)").style = "background-color: rgba(255, 255, 255, .5);";
+    document.querySelector("body div:nth-child(2) button:nth-child(2)").style = "background-color: #fc801a;";
+    document.querySelector("body div:nth-child(3) h1").style = "opacity:0";
+    setTimeout(() => {
+        document.querySelector("body div:nth-child(3) h1").innerText = "with a wide collection of cuisines";
+        document.querySelector("body div:nth-child(3) h1").style = "opacity:100";
+    }, 500);
+}, 1000);
+setTimeout(() => {
+    setTimeout(() => {
+        document.querySelector("div.container").style = "transform:translateX(-200%)";
+        document.querySelector("body div:nth-child(2) button:nth-child(1)").style = "background-color: rgba(255, 255, 255, .5);";
+        document.querySelector("body div:nth-child(2) button:nth-child(2)").style = "background-color: rgba(255, 255, 255, .5);";
+        document.querySelector("body div:nth-child(2) button:nth-child(3)").style = "background-color: #fc801a;";
+        document.querySelector("body div:nth-child(3) h1").style = "opacity:0";
+        setTimeout(() => {
+            document.querySelector("body div:nth-child(3) h1").innerText = "delivered quickly to your doorstep";
+            document.querySelector("body div:nth-child(3) h1").style = "opacity:100";
+        }, 500);
+    }, 1000);
+}, 2000);
+setTimeout(() => {
+    setTimeout(() => {
+        setTimeout(() => {
+            document.querySelector("div.container").style = "transform:translateX(0%)";
+            document.querySelector("body div:nth-child(2) button:nth-child(3)").style = "background-color: rgba(255, 255, 255, .5);";
+            document.querySelector("body div:nth-child(2) button:nth-child(2)").style = "background-color: rgba(255, 255, 255, .5);";
+            document.querySelector("body div:nth-child(2) button:nth-child(1)").style = "background-color: #fc801a;";
+            document.querySelector("body div:nth-child(3) h1").style = "opacity:0";
+            setTimeout(() => {
+                document.querySelector("body div:nth-child(3) h1").innerText = "Order from top & favourite restaurants";
+                document.querySelector("body div:nth-child(3) h1").style = "opacity:100";
+            }, 500);
+        }, 1000);
+    }, 2000);
+}, 2000);
+
+
+setInterval(() => {
+    setTimeout(() => {
+        document.querySelector("div.container").style = "transform:translateX(-100%)";
+        document.querySelector("body div:nth-child(2) button:nth-child(1)").style = "background-color: rgba(255, 255, 255, .5);";
+        document.querySelector("body div:nth-child(2) button:nth-child(3)").style = "background-color: rgba(255, 255, 255, .5);";
+        document.querySelector("body div:nth-child(2) button:nth-child(2)").style = "background-color: #fc801a;";
+        document.querySelector("body div:nth-child(3) h1").style = "opacity:0";
+        setTimeout(() => {
+            document.querySelector("body div:nth-child(3) h1").innerText = "with a wide collection of cuisines";
+            document.querySelector("body div:nth-child(3) h1").style = "opacity:100";
+        }, 500);
+    }, 1000);
+    setTimeout(() => {
+        setTimeout(() => {
+            document.querySelector("div.container").style = "transform:translateX(-200%)";
+            document.querySelector("body div:nth-child(2) button:nth-child(1)").style = "background-color: rgba(255, 255, 255, .5);";
+            document.querySelector("body div:nth-child(2) button:nth-child(2)").style = "background-color: rgba(255, 255, 255, .5);";
+            document.querySelector("body div:nth-child(2) button:nth-child(3)").style = "background-color: #fc801a;";
+            document.querySelector("body div:nth-child(3) h1").style = "opacity:0";
+            setTimeout(() => {
+                document.querySelector("body div:nth-child(3) h1").innerText = "delivered quickly to your doorstep";
+                document.querySelector("body div:nth-child(3) h1").style = "opacity:100";
+            }, 500);
+        }, 1000);
+    }, 2000);
+    setTimeout(() => {
+        setTimeout(() => {
+            setTimeout(() => {
+                document.querySelector("div.container").style = "transform:translateX(0%)";
+                document.querySelector("body div:nth-child(2) button:nth-child(3)").style = "background-color: rgba(255, 255, 255, .5);";
+                document.querySelector("body div:nth-child(2) button:nth-child(2)").style = "background-color: rgba(255, 255, 255, .5);";
+                document.querySelector("body div:nth-child(2) button:nth-child(1)").style = "background-color: #fc801a;";
+                document.querySelector("body div:nth-child(3) h1").style = "opacity:0";
+                setTimeout(() => {
+                    document.querySelector("body div:nth-child(3) h1").innerText = "Order from top & favourite restaurants";
+                    document.querySelector("body div:nth-child(3) h1").style = "opacity:100";
+                }, 500);
+            }, 1000);
+        }, 2000);
+    }, 2000);
+}, 9000);
