a6801498d67d09c0701bcefae0bc07b02f9ac622
Sambhu2612 <piyush.mitra87@gmail.com>
2021-09-12 14:31:32 +0530

    design completed for laptop screen

--- src/index.js
+++ src/index.js
@@ -84,3 +84,224 @@ setInterval(() => {
         }, 2000);
     }, 2000);
 }, 9000);
+
+
+
+// Laptop Screen
+setTimeout(() => {
+    document.querySelector("section.laptop header div.right_half").style = "opacity:100";
+}, 1000);
+setTimeout(() => {
+    document.querySelector("section.laptop header div.right_half").style = "opacity:0;";
+    document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_1.jpg')";
+    setTimeout(() => {
+        document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_2.jpg')";
+    }, 1000);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div.right_half").style = "opacity:100;background-Image:url('./src/img/header_img_2.jpg');";
+    }, 1500);
+}, 6000);
+
+setTimeout(() => {
+    document.querySelector("section.laptop header div.right_half").style = "opacity:0;";
+    document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_2.jpg')";
+    setTimeout(() => {
+        document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_3.jpg')";
+    }, 1000);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div.right_half").style = "opacity:100;";
+        document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_3.jpg')";
+    }, 1500);
+}, 12500);
+
+setTimeout(() => {
+    document.querySelector("section.laptop header div.right_half").style = "opacity:0;";
+    document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_3.jpg')";
+    setTimeout(() => {
+        document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_1.jpg')";
+    }, 1000);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div.right_half").style = "opacity:100;";
+        document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_1.jpg')";
+    }, 1500);
+}, 19000);
+
+
+setInterval(() => {
+    setTimeout(() => {
+        document.querySelector("section.laptop header div.right_half").style = "opacity:0;";
+        document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_1.jpg')";
+        setTimeout(() => {
+            document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_2.jpg')";
+        }, 1000);
+        setTimeout(() => {
+            document.querySelector("section.laptop header div.right_half").style = "opacity:100;background-Image:url('./src/img/header_img_2.jpg');";
+        }, 1500);
+    }, 6000);
+    
+    setTimeout(() => {
+        document.querySelector("section.laptop header div.right_half").style = "opacity:0;";
+        document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_2.jpg')";
+        setTimeout(() => {
+            document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_3.jpg')";
+        }, 1000);
+        setTimeout(() => {
+            document.querySelector("section.laptop header div.right_half").style = "opacity:100;";
+            document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_3.jpg')";
+        }, 1500);
+    }, 12500);
+    
+    setTimeout(() => {
+        document.querySelector("section.laptop header div.right_half").style = "opacity:0;";
+        document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_3.jpg')";
+        setTimeout(() => {
+            document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_1.jpg')";
+        }, 1000);
+        setTimeout(() => {
+            document.querySelector("section.laptop header div.right_half").style = "opacity:100;";
+            document.querySelector("section.laptop header div.right_half").style.backgroundImage = "url('./src/img/header_img_1.jpg')";
+        }, 1500);
+    }, 19000);
+}, 19500);
+
+
+// h1 animation
+
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(-40%);opacity:0";
+}, 3000);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(40%);opacity:0;transition:0s ease;";
+}, 3500);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").innerText = "Late Night At Office ?";
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(0%);opacity:100";
+}, 3550);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(-40%);opacity:0";
+}, 7050);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(40%);opacity:0;transition:0s ease;";
+}, 7550);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").innerText = "Hungry ?";
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(0%);opacity:100";
+}, 7600);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(-40%);opacity:0";
+}, 11100);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(40%);opacity:0;transition:0s ease;";
+}, 11600);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").innerText = "Unexpected guests ?";
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(0%);opacity:100";
+}, 11650);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(-40%);opacity:0";
+}, 15150);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(40%);opacity:0;transition:0s ease;";
+}, 15650);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").innerText = "Cooking gone wrong ?";
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(0%);opacity:100";
+}, 15700);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(-40%);opacity:0";
+}, 19200);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(40%);opacity:0;transition:0s ease;";
+}, 19700);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").innerText = "Movie Night ?";
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(0%);opacity:100";
+}, 19750);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(-40%);opacity:0";
+}, 23250);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(40%);opacity:0;transition:0s ease;";
+}, 23750);
+setTimeout(() => {
+    document.querySelector("section.laptop header div:nth-child(1) h1").innerText = "Game Night ?";
+    document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(0%);opacity:100";
+}, 23800);
+
+
+setInterval(() => {
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(-40%);opacity:0";
+    }, 3000);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(40%);opacity:0;transition:0s ease;";
+    }, 3500);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").innerText = "Late Night At Office ?";
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(0%);opacity:100";
+    }, 3550);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(-40%);opacity:0";
+    }, 7050);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(40%);opacity:0;transition:0s ease;";
+    }, 7550);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").innerText = "Hungry ?";
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(0%);opacity:100";
+    }, 7600);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(-40%);opacity:0";
+    }, 11100);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(40%);opacity:0;transition:0s ease;";
+    }, 11600);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").innerText = "Unexpected guests ?";
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(0%);opacity:100";
+    }, 11650);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(-40%);opacity:0";
+    }, 15150);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(40%);opacity:0;transition:0s ease;";
+    }, 15650);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").innerText = "Cooking gone wrong ?";
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(0%);opacity:100";
+    }, 15700);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(-40%);opacity:0";
+    }, 19200);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(40%);opacity:0;transition:0s ease;";
+    }, 19700);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").innerText = "Movie Night ?";
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(0%);opacity:100";
+    }, 19750);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(-40%);opacity:0";
+    }, 23250);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(40%);opacity:0;transition:0s ease;";
+    }, 23750);
+    setTimeout(() => {
+        document.querySelector("section.laptop header div:nth-child(1) h1").innerText = "Game Night ?";
+        document.querySelector("section.laptop header div:nth-child(1) h1").style = "transform:translateY(0%);opacity:100";
+    }, 23800);
+}, 24300);
+
+// input outline
+
+document.querySelector("section.laptop header div:nth-child(1) div:nth-child(4) div:nth-child(1) input").addEventListener("click",(event)=>{
+    event.target.parentElement.style = "border: 1px solid #FC8019 !important;box-shadow: 0 1px 10px 0 rgba(40,44,63,.1)";
+    event.stopPropagation();
+});
+
+document.querySelector("body").addEventListener("click",(event)=>{
+    document.querySelector("section.laptop header div:nth-child(1) div:nth-child(4) div:nth-child(1)").style = "border: 1px solid #bebfc5;";
+});
+
+document.querySelector("section.laptop header div:nth-child(1) div:nth-child(4) div:nth-child(1) input").click();
+document.querySelector("section.laptop header div:nth-child(1) div:nth-child(4) div:nth-child(1) input").focus();
